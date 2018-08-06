6370d4cdb3040f03f79acffb2ffd8d27d80f80e9
liugangfeng <liugangfeng@henrongyi.com>
2018-08-06 18:02:37 +0800

    .

--- src/main/java/com/hw/HelloWorldController.java
+++ src/main/java/com/hw/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping("/hello")
 	public String say() {
-		return "hello world!";
+		return "test !!!  hello world!";
 	}
 
 }
